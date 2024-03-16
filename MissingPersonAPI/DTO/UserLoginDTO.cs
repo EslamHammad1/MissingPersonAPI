c1446c6e4a72e556c07227e844404d3ecab96b4e
20912020100648 <20912020100648@fci.zu.edu.eg>
2024-03-16 17:21:34 +0200

    #push

--- MissingPersonAPI/DTO/UserLoginDTO.cs
+++ MissingPersonAPI/DTO/UserLoginDTO.cs
@@ -2,9 +2,7 @@
 {
     public class UserLoginDTO
     {
-        [Required]
         public string? UserName { get; set; } 
-        [Required]
         [DataType(DataType.Password)]
         public string? Password { get; set; }
    
--- MissingPersonAPI/DTO/UserRegisterDTO.cs
+++ MissingPersonAPI/DTO/UserRegisterDTO.cs
@@ -2,13 +2,10 @@
 {
     public class UserRegisterDTO
     {
-        [Required]
         public string? UserName { get; set; } 
         public string? Name { get; set; } 
-        [Required]
         [DataType(DataType.Password)]
         public string? Password { get; set; } 
-        [Required]
         [Compare("Password")]
         [DataType(DataType.Password)]
         public string? PasswordConfirmed { get; set; } 
